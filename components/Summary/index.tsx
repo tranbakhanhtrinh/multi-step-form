7b3e39f330387deab643d307e0bd1cf7d3bda83e
tranbakhanhtrinh <tranbakhanhtrinh@gmail.com>
2023-08-19 22:05:59 +0700

    prevent getting all states.

--- components/Summary/index.tsx
+++ components/Summary/index.tsx
@@ -9,7 +9,8 @@ import { useAppSelector } from '@/redux/hooks'
 
 const Summary = () => {
   const { stepName, setStepName } = useContext(MenuContext)
-  const { plan, addons } = useAppSelector(state => state)
+  const { data: plan } = useAppSelector(state => state.plan)
+  const { data: addons } = useAppSelector(state => state.addons)
   return (
     <>
       {stepName === StepName.SUMMARY && <>
@@ -17,7 +18,7 @@ const Summary = () => {
         <p className='font-medium text-grey text-base mb-9'>
           Double-check everything looks OK before confirming.
         </p>
-        <SummaryTable plan={plan.data.plan} price={plan.data.price} addonsArr={addons.data} />
+        <SummaryTable plan={plan.plan} price={plan.price} addonsArr={addons} />
         <div className='flex justify-between items-center bottom-0 lg:absolute fixed bottom-0 right-0 p-4 lg:p-0 bg-white w-full'>
           <Button type='button' className='text-grey font-medium text-base' onClick={() => setStepName(StepName.ADDONS)}>Go Back</Button>
           <Button type="submit" className="bg-purple rounded-md py-4 px-6 text-white font-medium" onClick={() => setStepName(StepName.THANK)}>
